5803fa122c40f10724f5387c3f5feaded0424b5e
Felipe Romero <feliperomero3@outlook.com>
2022-05-19 21:12:36 -0500

    Add Set-ArchiveFilePath function.

---
+++ src/Set-ArchiveFilePath.ps1
@@ -0,0 +1,56 @@
+<#
+.SYNOPSIS
+    A simple script to clean up old log files.
+.DESCRIPTION
+    Finds the files to archive, adds the old files to an archive file, then removes the old files.
+.NOTES
+    Work in progress.
+#>
+
+function Set-ArchiveFilePath {
+    <#
+    .SYNOPSIS
+        Creates a file path.
+    .DESCRIPTION
+        Creates a file path for an archive file with .zip extension.
+        Takes a prefix for the archive file and appends it a timestamp e. g. NewArchive20220519.zip
+    .PARAMETER ZipPath
+        The path to store the archive.
+    .PARAMETER ZipPrefix
+        The prefix for the archive.
+    .PARAMETER Date
+        The date value to use for creating the timestamp.
+    .OUTPUTS
+        The archive file path.
+    #>
+    [CmdletBinding()]
+    [OutputType([String])]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ZipPath,
+        
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ZipPrefix,
+
+        [Parameter(Mandatory = $true)]
+        [datetime]
+        $Date
+    )
+
+    if (-not(Test-Path -Path $ZipPath)) {
+        New-Item -Path $ZipPath -ItemType Directory | Out-Null
+        Write-Verbose -Message "Created folder '$ZipPath'."
+    }
+
+    $timeString = $Date.ToString('yyyyMMdd')
+    $zipName = "$($ZipPrefix)$($timeString).zip"
+    $zipFile = Join-Path -Path $ZipPath -ChildPath $zipName
+
+    if (Test-Path -Path $zipFile) {
+        throw "The file '$zipFile' already exists."
+    }
+
+    $zipFile
+}
